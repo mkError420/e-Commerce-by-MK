2d71125fc0b9c6c06baca872020ee9686428c6e1
MK Rabbani <133435149+mkError420@users.noreply.github.com>
2026-01-02 16:51:09 +0600

    final setup

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <h2 className='text-xl font-semibold'>Home</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ex suscipit cupiditate nisi ipsum enim, iure labore animi nam laboriosam reiciendis quis voluptas nesciunt itaque exercitationem voluptatum consequatur! Fugit expedita hic sint. Voluptate nisi earum temporibus, quisquam animi atque natus hic sed, in totam ab. Iste quidem laborum quibusdam odit?
       </p>
-      <Button size="lg">Check out </Button>
+      <Button size="lg"><h3>Check out</h3> </Button>
     </div>
   )
 }
