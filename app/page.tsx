1f8ba784ce5c20410cd272292d453c49348aeef4
MK Rabbani <133435149+mkError420@users.noreply.github.com>
2026-01-02 15:03:09 +0600

    Setum language

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home
+      <h2>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ex suscipit cupiditate nisi ipsum enim, iure labore animi nam laboriosam reiciendis quis voluptas nesciunt itaque exercitationem voluptatum consequatur! Fugit expedita hic sint. Voluptate nisi earum temporibus, quisquam animi atque natus hic sed, in totam ab. Iste quidem laborum quibusdam odit?</p>
     </div>
   )
 }
 
-export default Home
+export default Home;
