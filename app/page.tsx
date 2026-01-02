1ad9ab2854f13b1fdc3bfa26e4bb94ee41c5741c
MK Rabbani <133435149+mkError420@users.noreply.github.com>
2026-01-02 16:30:57 +0600

    setup all sadcn and tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from '@/components/ui/button';
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h2>Home</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ex suscipit cupiditate nisi ipsum enim, iure labore animi nam laboriosam reiciendis quis voluptas nesciunt itaque exercitationem voluptatum consequatur! Fugit expedita hic sint. Voluptate nisi earum temporibus, quisquam animi atque natus hic sed, in totam ab. Iste quidem laborum quibusdam odit?</p>
+    <div className='p-10 bg-shop-light-pink'>
+      <h2 className='text-xl font-semibold'>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ex suscipit cupiditate nisi ipsum enim, iure labore animi nam laboriosam reiciendis quis voluptas nesciunt itaque exercitationem voluptatum consequatur! Fugit expedita hic sint. Voluptate nisi earum temporibus, quisquam animi atque natus hic sed, in totam ab. Iste quidem laborum quibusdam odit?
+      </p>
+      <Button size="lg">Check out </Button>
     </div>
   )
 }
