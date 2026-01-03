b7f998f059a8d197dc9d48c380ffdc4cd5774730
MK Rabbani <133435149+mkError420@users.noreply.github.com>
2026-01-03 15:59:10 +0600

    Resolved header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Container from './Container';
 import Logo from './Logo';
 import HeaderMenu from './HeaderMenu';
+import SearchBar from './SearchBar';
+import CartIcon from './CartIcon';
+import FavoriteButton from './FavoriteButton';
+import SignIn from './SignIn';
 
 const Header = () => {
   return (
@@ -11,8 +15,11 @@ const Header = () => {
         <Logo/>
         {/* Navbutton */}
         <HeaderMenu/>
-        <div>
-          Others
+        <div className='w-auto md:w-1/3 flex items-center justify-end gap-5 '>
+          <SearchBar/>
+          <CartIcon/>
+          <FavoriteButton/>
+          <SignIn/>
         </div>
       </Container>
     </header>
