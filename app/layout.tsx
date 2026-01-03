2ef75997c268d5426d643d29f694ae0c758890b7
MK Rabbani <133435149+mkError420@users.noreply.github.com>
2026-01-04 02:07:08 +0600

    Sidebar fully complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,10 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
   <body className="font-poppins antialiased">
-  <Header/>
-  {children}
-  <Footer/>
-    </body>
-    </html>
+      <Header/>
+      {children}
+      <Footer/>
+  </body>
+  </html>
   );
 }
--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -3,18 +3,26 @@ import Logo from './Logo';
 import { X } from 'lucide-react';
 import Link from 'next/link';
 import { headerData } from '@/constants/data';
+import { usePathname } from 'next/navigation';
+import SocialMedia from './SocialMedia';
+import { useOutsideClick } from '@/hooks';
 interface SidebarProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
-const SideMenu:FC<SidebarProps> = ({isOpen, onClose}) => {
+  const SideMenu:FC<SidebarProps> = ({isOpen, onClose}) => {
+  const pathname = usePathname();
+
+
+  const sidebarRef=useOutsideClick<HTMLDivElement>(onClose)
   return (
-    <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/80 shadow-xl
+    <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/70 shadow-xl
     ${isOpen?"translate-x-0":"-translate-x-full"
     } hoverEffect`}
   >
-      <div className='min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6'>
+      <div ref={sidebarRef} 
+        className='min-w-72 max-w-76 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6'>
         <div className='flex items-center justify-between gap-5'>
           <Logo className='text-white' spanDesign="group-hover:text-white" />
           <button onClick={onClose} className='hover:text-shop_light_green hoverEffect'>
@@ -22,16 +30,17 @@ const SideMenu:FC<SidebarProps> = ({isOpen, onClose}) => {
           </button>
         </div>
 
-        <div>
+        <div className='flex flex-col space-y-3.5 font-semibold tracking-wide'>
           {headerData?.map((item)=>(
-           <Link href={item?.link} key={item?.title}>
+           <Link href={item?.link} key={item?.title} className={`hover:text-shop_light_green hoverEffect ${pathname === item?.link && 'text-shop_light_green/70'}`}>
            {item.title}
            </Link>
           ))}
         </div>
+        <SocialMedia/>
       </div>
     </div>
-  )
-}
+ );
+};
 
 export default SideMenu;
