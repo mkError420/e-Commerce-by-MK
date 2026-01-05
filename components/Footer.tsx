35ae3d1b9eb0af8eb008ba66bac76c14d53554b0
MK Rabbani <133435149+mkError420@users.noreply.github.com>
2026-01-05 15:54:14 +0600

    Footer 1st commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import Container from './Container'
 import FooterTop from './FooterTop'
+import Logo from './Logo'
+import SocialMedia from './SocialMedia'
 
 const Footer = () => {
   return (
     <footer className='bg-white border-t'>
       <Container>
           <FooterTop/>
+          <div>
+            <div>
+              <Logo/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus deserunt quis eum asperiores, voluptatibus nam!</p>
+              <SocialMedia className='text-darkColor/60' iconClassName='border-darkColor/60 hover:border-shop_light_green hover:text-shop_light_green'
+              tooltipClassName='bg-darkColor text-white'
+              />
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
       </Container> 
     </footer>
   )
